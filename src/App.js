192111d96d2003bc97560067705c84b0737feb70
unknown <talhanadeem.dev@gmail.com>
2021-08-10 09:29:10 +0500

    my first comit reactwebsite

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import {useState, useEffect} from 'react';
+import Player from './components/Player/Player';
+
+function App() {
+  const [songs] = useState([
+    {
+      title: "Forget me too ft. Halsey",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song-1.jpg",
+      src: "./music/on-n-on.mp3"
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: "./music/somebody-new.mp3"
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3",
+      img_src: "./images/song-3.jpg",
+      src: "./music/on-n-on.mp3"
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 4",
+      img_src: "./images/song-4.jpg",
+      src: "./music/somebody-new.mp3"
+    },
+    {
+      title: "Tera Yaar Hoon",
+      artist: "Artist 5",
+      img_src: "./images/song-5.jpg",
+      src: "./music/Tera Yaar Hoon.m4a"
+    }
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
+  return (
+    <div className="App">
+      <Player 
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
+      />
+    </div>
+  );
+}
+
+export default App;
